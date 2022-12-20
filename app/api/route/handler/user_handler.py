4a56cb2b832911f549fe9b904f8f93843439cf0a
Loren Yu <loren@navapbc.com>
2022-12-20 11:58:23 -0800

    Remove unused dataclasses

--- app/api/route/handler/user_handler.py
+++ app/api/route/handler/user_handler.py
@@ -1,5 +1,3 @@
-import uuid
-from datetime import date
 from typing import Optional
 from uuid import uuid4
 
@@ -7,41 +5,11 @@ import apiflask
 from sqlalchemy import orm
 
 import api.logging
-from api.db.models.user_models import Role, RoleType, User
+from api.db.models.user_models import Role, User
 from api.route.api_context import ApiContext
-from api.route.request import BaseRequestModel
 from api.route.route_utils import get_or_404
 
 logger = api.logging.get_logger(__name__)
-
-
-class RoleParams(BaseRequestModel):
-    type: RoleType
-
-
-class UserParams(BaseRequestModel):
-    id: Optional[uuid.UUID]
-    first_name: str
-    middle_name: Optional[str]
-    last_name: str
-    phone_number: str
-    date_of_birth: date
-    is_active: bool
-    roles: Optional[list[RoleParams]]
-
-
-class UserPatchParams(BaseRequestModel):
-    first_name: Optional[str]
-    middle_name: Optional[str]
-    last_name: Optional[str]
-    phone_number: Optional[str]
-    date_of_birth: Optional[date]
-    is_active: Optional[bool]
-    roles: Optional[list[RoleParams]]
-
-
-class UserResponse(UserParams):
-    pass
 
 
 def create_user(user_data: dict, api_context: ApiContext) -> User:
