ca35a50f90708df717fc158d390f40fe7f6f23e0
Jesse Shepherd <jesseshepherd@navapbc.com>
2022-12-19 17:00:40 -0800

    lint

--- app/api/services/users/__init__.py
+++ app/api/services/users/__init__.py
@@ -1,3 +1,3 @@
-from .create_user import create_user
-from .get_user import get_user
-from .patch_user import patch_user
+from .create_user import create_user  # noqa: F401
+from .get_user import get_user  # noqa: F401
+from .patch_user import patch_user  # noqa: F401
