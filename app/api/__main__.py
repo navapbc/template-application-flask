8732c2ebe694b5eefad712fc0272f41317679ab4
Loren Yu <loren@navapbc.com>
2022-12-21 09:12:03 -0800

    Set port from environment variable (#41)
    * Ignore VScode files
    * Set server port from environment variable

--- app/api/__main__.py
+++ app/api/__main__.py
@@ -21,14 +21,15 @@ def main() -> None:
     logger.info("Running API Application")
 
     app = api.app.create_app()
+    port = app_config.port
 
     if app_config.environment == "local":
         # If python files are changed, the app will auto-reload
         # Note this doesn't have the OpenAPI yaml file configured at the moment
-        app.run(port=8080, use_reloader=True, reloader_type="stat")
+        app.run(port=port, use_reloader=True, reloader_type="stat")
     else:
         # Don't enable the reloader if non-local
-        app.run(port=8080)
+        app.run(port=port)
 
 
 main()
