815ab13addbe6b8c2fe7654af651d424941f6483
Jesse Shepherd <jesseshepherd@navapbc.com>
2022-12-22 11:21:07 -0800

    Add TODO

--- app/api/services/users/create_user.py
+++ app/api/services/users/create_user.py
@@ -6,6 +6,8 @@ from api.route.api_context import ApiContext
 
 # TODO: separate controller and service concerns
 # https://github.com/navapbc/template-application-flask/issues/49#issue-1505008251
+# TODO: Use classes / objects as inputs to service methods
+# https://github.com/navapbc/template-application-flask/issues/52
 def create_user(user_data: dict, api_context: ApiContext) -> User:
     # TODO: move this code to service and/or persistence layer
     user = User(**user_data)
--- app/api/services/users/patch_user.py
+++ app/api/services/users/patch_user.py
@@ -7,6 +7,8 @@ from api.route.route_utils import get_or_404
 
 # TODO: separate controller and service concerns
 # https://github.com/navapbc/template-application-flask/issues/49#issue-1505008251
+# TODO: Use classes / objects as inputs to service methods
+# https://github.com/navapbc/template-application-flask/issues/52
 def patch_user(user_id: str, patch_data: dict, api_context: ApiContext) -> User:
     user = get_or_404(api_context.db_session, User, user_id)
 
