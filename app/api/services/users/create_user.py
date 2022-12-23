4f81c53173bd1293e6e3262b8d2b8388caf5f8c2
Loren Yu <loren@navapbc.com>
2022-12-23 13:07:05 -0800

    Use RequestUser in create_user

--- app/api/services/users/create_user.py
+++ app/api/services/users/create_user.py
@@ -2,15 +2,16 @@ from uuid import uuid4
 
 from api.db.models.user_models import User
 from api.route.api_context import ApiContext
+from api.route.schemas import user_schemas
 
 
 # TODO: separate controller and service concerns
 # https://github.com/navapbc/template-application-flask/issues/49#issue-1505008251
 # TODO: Use classes / objects as inputs to service methods
 # https://github.com/navapbc/template-application-flask/issues/52
-def create_user(user_data: dict, api_context: ApiContext) -> User:
+def create_user(request_user: user_schemas.RequestUser, api_context: ApiContext) -> User:
     # TODO: move this code to service and/or persistence layer
-    user = User(**user_data)
+    user = User(**request_user.as_dict())
     user.id = uuid4()
 
     if user.roles is not None:
