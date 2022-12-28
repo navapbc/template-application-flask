78ff36ec76815165120034eef9645678f93bdad0
Jesse Shepherd <jesseshepherd@navapbc.com>
2022-12-28 13:23:36 -0800

    Clarify comment

--- app/api/services/models/base.py
+++ app/api/services/models/base.py
@@ -1,6 +1,6 @@
 # Base class for handling input params for PATCH endpoints.
 # Persist the original JSON input in _fields_set, so that
-# we can determine which values were set by the user, and
+# we can determine which fields were set by the user, and
 # which were omitted.
 class PatchParamsBase:
     _fields_set: dict = {}
