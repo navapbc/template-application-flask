a4bf1d5508f2589ef97c342c42b5187ff6f9c62a
Loren Yu <loren@navapbc.com>
2023-01-10 13:18:28 -0800

    Make powerset string docstring

--- app/tests/api/route/test_user_route.py
+++ app/tests/api/route/test_user_route.py
@@ -28,7 +28,7 @@ def base_request():
 
 
 def powerset(iterable):
-    "powerset([1,2,3]) --> [] [1] [2] [3] [1,2] [1,3] [2,3] [1,2,3]"
+    """powerset([1,2,3]) --> [] [1] [2] [3] [1,2] [1,3] [2,3] [1,2,3]"""
     s = list(iterable)
     return map(list, chain.from_iterable(combinations(s, r) for r in range(len(s) + 1)))
 
