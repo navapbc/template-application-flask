95e6e4a542b69ae40a7ed54ff47a916c5320a517
Loren Yu <loren@navapbc.com>
2023-01-12 08:58:16 -0800

    Add comment on metadata

--- app/api/db/models/__init__.py
+++ app/api/db/models/__init__.py
@@ -5,6 +5,7 @@ from . import base, user_models
 logger = api.logging.get_logger(__name__)
 
 # Re-export metadata
+# This is used by tests to create the test database.
 metadata = base.metadata
 
 __all__ = ["metadata", "user_models"]
