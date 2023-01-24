86eceab57f6124c82810ea72b151dabe71b2afdd
Loren Yu <loren@navapbc.com>
2023-01-24 14:37:00 -0800

    Remove test for repeated message

--- app/tests/api/logging/test_audit.py
+++ app/tests/api/logging/test_audit.py
@@ -28,36 +28,3 @@ def test_audit_hook(caplog):
     assert caplog.records[0].__dict__["audit.args.path"] == "/dev/null"
     assert caplog.records[0].__dict__["audit.args.mode"] is None
     assert caplog.records[0].__dict__["audit.args.flags"] == 123000
-
-
-def test_audit_log_repeated_message(caplog):
-    caplog.set_level(logging.INFO)
-
-    for _i in range(500):
-        audit.audit_log("abc", (1, 2, 3))
-
-    assert [(r.funcName, r.levelname, r.message, r.count) for r in caplog.records] == [
-        ("audit_log", "AUDIT", "abc", 1),
-        ("audit_log", "AUDIT", "abc", 2),
-        ("audit_log", "AUDIT", "abc", 3),
-        ("audit_log", "AUDIT", "abc", 4),
-        ("audit_log", "AUDIT", "abc", 5),
-        ("audit_log", "AUDIT", "abc", 6),
-        ("audit_log", "AUDIT", "abc", 7),
-        ("audit_log", "AUDIT", "abc", 8),
-        ("audit_log", "AUDIT", "abc", 9),
-        ("audit_log", "AUDIT", "abc", 10),
-        ("audit_log", "AUDIT", "abc", 20),
-        ("audit_log", "AUDIT", "abc", 30),
-        ("audit_log", "AUDIT", "abc", 40),
-        ("audit_log", "AUDIT", "abc", 50),
-        ("audit_log", "AUDIT", "abc", 60),
-        ("audit_log", "AUDIT", "abc", 70),
-        ("audit_log", "AUDIT", "abc", 80),
-        ("audit_log", "AUDIT", "abc", 90),
-        ("audit_log", "AUDIT", "abc", 100),
-        ("audit_log", "AUDIT", "abc", 200),
-        ("audit_log", "AUDIT", "abc", 300),
-        ("audit_log", "AUDIT", "abc", 400),
-        ("audit_log", "AUDIT", "abc", 500),
-    ]
