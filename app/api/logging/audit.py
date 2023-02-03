7916b8ffbe8b87835ab171c3dee60e38cc2d806d
Loren Yu <loren@navapbc.com>
2023-02-02 16:24:36 -0800

    Specify which audit events/args to log rather than events to ignore (#101)
    In [this PR
    comment](https://github.com/navapbc/template-application-flask/pull/97#pullrequestreview-1266020535),
    @chouinar explained that he noticed env vars were being logged by
    api.logging.audit module. This PR changes the logging module to specific
    which events to log rather than which events to ignore, and adds tests for ensuring we don't log sensitive data

--- app/api/logging/audit.py
+++ app/api/logging/audit.py
@@ -8,65 +8,91 @@
 
 import logging
 import sys
-from typing import Any
+from typing import Any, Sequence
 
 import api.util.collections
 
 logger = logging.getLogger(__name__)
 
 AUDIT = 32
-
+logging.INFO
 logging.addLevelName(AUDIT, "AUDIT")
 
 
-def init_security_logging() -> None:
-    sys.addaudithook(audit_hook)
+def init() -> None:
+    """Initialize the audit logging module to start
+    logging security audit events."""
+    sys.addaudithook(handle_audit_event)
 
 
-IGNORE_AUDIT_EVENTS = {
-    "builtins.id",
-    "code.__new__",
-    "compile",
-    "exec",
-    "import",
-    "marshal.loads",
-    "object.__getattr__",
-    "object.__setattr__",
-    "os.listdir",
-    "os.scandir",
-    "os.walk",
-    "socket.__new__",
-    "sys._current_frames",
-    "sys._getframe",
-    "sys.settrace",  # interferes with PyCharm debugger
-}
+def handle_audit_event(event_name: str, args: tuple[Any, ...]) -> None:
+    # Define events to log and the arguments to log for each event.
+    # For more information about these events and what they mean, see https://peps.python.org/pep-0578/#suggested-audit-hook-locations
+    # For the full list of auditable events, see https://docs.python.org/3/library/audit_events.html
+    # Define this variable locally so it can't be modified by other modules.
 
+    EVENTS_TO_LOG = {
+        # Detect dynamic execution of code objects. This only occurs for explicit
+        # calls, and is not raised for normal function invocation.
+        "exec": ("code_object",),
+        # Detect when a file is about to be opened. path and mode are the usual
+        # parameters to open if available, while flags is provided instead of
+        # mode in some cases.
+        "open": ("path", "mode", "flags"),
+        # Detect when a signal is sent to a process.
+        "os.kill": ("pid", "sig"),
+        # Detect when a file is renamed.
+        "os.rename": ("src", "dst", "src_dir_fd", "dst_dir_fd"),
+        # Detect when a subprocess is started.
+        "subprocess.Popen": ("executable", "args", "cwd", "_"),
+        # Detect access to network resources. The address is unmodified from the original call.
+        "socket.connect": ("socket", "address"),
+        "socket.getaddrinfo": ("host", "port", "family", "type", "protocol"),
+        # Detect when new audit hooks are being added.
+        "sys.addaudithook": (),
+        # Detects URL requests.
+        # Don't log data or headers because they may contain sensitive information.
+        "urllib.Request": ("url", "_", "_", "method"),
+    }
 
-def audit_hook(event_name: str, args: tuple[Any, ...]) -> None:
-    if event_name in IGNORE_AUDIT_EVENTS:
+    if event_name not in EVENTS_TO_LOG:
         return
-    if event_name == "open" and isinstance(args[0], str) and "/__pycache__/" in args[0]:
-        # Python has a high rate of these events in normal operation.
-        return
-    if event_name == "os.chmod" and type(args[0]) is int:
-        # Gunicorn generates a high volume of these events in normal operation (see workertmp.py)
-        return
-    if event_name == "open" and isinstance(args[0], str) and "/pytz/" in args[0]:
-        # The pytz module generates a high volume of these events in normal operation.
-        return
 
-    audit_log(event_name, args)
+    arg_names = EVENTS_TO_LOG[event_name]
+    log_audit_event(event_name, args, arg_names)
 
 
-def audit_log(event_name: str, args: tuple[Any, ...]) -> None:
+# Set the audit hook to be traceable so that coverage module can track calls to it
+# The coverage module relies on Python's trace hooks
+# (See https://coverage.readthedocs.io/en/7.1.0/howitworks.html#execution)
+# According to the docs for sys.addaudithook, the audit hook is only traced if the callable
+# has a __cantrace__ member that is set to a true value.
+# (See https://docs.python.org/3/library/sys.html#sys.addaudithook)
+handle_audit_event.__cantrace__ = True  # type: ignore
+
+
+def log_audit_event(event_name: str, args: Sequence[Any], arg_names: Sequence[str]) -> None:
     """Log a message but only log recently repeated messages at intervals."""
+    extra = {
+        f"audit.args.{arg_name}": arg for arg_name, arg in zip(arg_names, args) if arg_name != "_"
+    }
+
     key = (event_name, repr(args))
-    count = audit_message_count[key] = audit_message_count[key] + 1
-    if count <= 10 or (count <= 100 and (count % 10) == 0) or (count % 100) == 0:
-        extra = {"audit.event_name": event_name, "count": count}
-        for arg_index, arg in enumerate(args):
-            extra["audit.args.%i" % arg_index] = arg
-        logger.log(AUDIT, event_name, extra=extra)
+    if key not in audit_message_count:
+        count = 1
+    else:
+        count = audit_message_count[key] + 1
+    audit_message_count[key] = count
+
+    if count > 100 and count % 100 != 0:
+        return
+
+    if count > 10 and count % 10 != 0:
+        return
+
+    extra["count"] = count
+
+    logger.log(AUDIT, event_name, extra=extra)
 
 
 audit_message_count = api.util.collections.LeastRecentlyUsedDict()
--- app/tests/api/logging/test_audit.py
+++ app/tests/api/logging/test_audit.py
@@ -3,62 +3,233 @@
 #
 
 import logging
+import os
+import pathlib
+import signal
+import socket
+import subprocess
+import sys
+import urllib.error
+import urllib.parse
+import urllib.request
+from typing import Any, Callable
+
+import pytest
 
 import api.logging.audit as audit
 
 
-def test_audit_hook(caplog):
+@pytest.fixture(scope="session")
+def init_audit_hook():
+    audit.init()
+
+
+test_audit_hook_data = [
+    pytest.param(eval, ("1+1", None, None), [{"msg": "exec"}], id="eval"),
+    pytest.param(exec, ("1+1", None, None), [{"msg": "exec"}], id="exec"),
+    pytest.param(
+        open,
+        ("/dev/null", "w"),
+        [
+            {
+                "msg": "open",
+                "audit.args.path": "/dev/null",
+                "audit.args.mode": "w",
+                "audit.args.flags": 524865,
+            }
+        ],
+        id="open",
+    ),
+    pytest.param(
+        os.kill,
+        (-1, signal.SIGTERM),  # Using PID=-1 since it should not ever be a valid PID
+        [
+            {
+                "msg": "os.kill",
+                "audit.args.pid": -1,
+                "audit.args.sig": signal.SIGTERM,
+            }
+        ],
+        id="os.kill",
+    ),
+    pytest.param(
+        os.rename,
+        ("/tmp/oldname", "/tmp/newname"),
+        [
+            {
+                "msg": "os.rename",
+                "audit.args.src": "/tmp/oldname",
+                "audit.args.dst": "/tmp/newname",
+            }
+        ],
+        id="os.rename",
+    ),
+    pytest.param(
+        subprocess.Popen,
+        (["/usr/bin/git", "commit", "-m", "Fixes a bug."],),
+        [
+            {
+                "msg": "subprocess.Popen",
+                "audit.args.executable": "/usr/bin/git",
+                "audit.args.args": ["/usr/bin/git", "commit", "-m", "Fixes a bug."],
+            }
+        ],
+        id="subprocess.Popen",
+    ),
+    pytest.param(
+        os.open,
+        ("/dev/null", os.O_RDWR | os.O_CREAT, 0o777),
+        [
+            {
+                "msg": "open",
+                "audit.args.path": "/dev/null",
+                "audit.args.mode": None,
+                "audit.args.flags": 524354,
+            }
+        ],
+        id="os.open",
+    ),
+    pytest.param(
+        sys.addaudithook,
+        (lambda *args: None,),
+        [{"msg": "sys.addaudithook"}],
+        id="sys.addaudithook",
+    ),
+    pytest.param(
+        socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect,
+        (("www.python.org", 80),),
+        [{"msg": "socket.connect", "audit.args.address": ("www.python.org", 80)}],
+        id="socket.connect",
+    ),
+    pytest.param(
+        socket.getaddrinfo,
+        ("www.python.org", 80),
+        [{"msg": "socket.getaddrinfo", "audit.args.host": "www.python.org", "audit.args.port": 80}],
+        id="socket.getaddrinfo",
+    ),
+    pytest.param(
+        urllib.request.urlopen,
+        ("https://www.python.org",),
+        # urllib.request.urlopen calls socket.getaddrinfo and socket.connect under the hood,
+        # both of which trigger audit log entries
+        [
+            {
+                "msg": "urllib.Request",
+                "audit.args.url": "https://www.python.org",
+                "audit.args.method": "GET",
+            },
+            {
+                "msg": "socket.getaddrinfo",
+                "audit.args.host": "www.python.org",
+                "audit.args.port": 443,
+            },
+            {
+                "msg": "socket.connect",
+            },
+        ],
+        id="urllib.request.urlopen",
+    ),
+]
+
+
+@pytest.mark.parametrize("func,args,expected_records", test_audit_hook_data)
+def test_audit_hook(
+    init_audit_hook,
+    caplog: pytest.LogCaptureFixture,
+    func: Callable,
+    args: tuple[Any],
+    expected_records: list[dict[str, Any]],
+):
     caplog.set_level(logging.INFO)
+    caplog.clear()
 
-    # Should appear in audit log.
-    audit.audit_hook("open", ("/dev/null", None, 123000))
+    try:
+        func(*args)
+    except Exception:
+        pass
 
-    # Various common cases that should not appear in audit log (normal behaviour & too noisy).
-    audit.audit_hook("compile", (b"def _(): pass", "<unknown>"))
-    audit.audit_hook("open", ("/srv/api/__pycache__/status.cpython-310.pyc", "r", 500010))
-    audit.audit_hook("os.chmod", (7, 1, -1))
-    audit.audit_hook(
-        "open", ("/app/.venv/lib/python3.10/site-packages/pytz/zoneinfo/US/Eastern", "r", 524288)
-    )
-
-    assert [(r.funcName, r.levelname, r.message) for r in caplog.records] == [
-        ("audit_log", "AUDIT", "open")
-    ]
-    assert caplog.records[0].__dict__["audit.event_name"] == "open"
-    assert caplog.records[0].__dict__["audit.args.0"] == "/dev/null"
-    assert caplog.records[0].__dict__["audit.args.1"] is None
-    assert caplog.records[0].__dict__["audit.args.2"] == 123000
-    assert "audit.args.3" not in caplog.records[0].__dict__
+    assert len(caplog.records) == len(expected_records)
+    for record, expected_record in zip(caplog.records, expected_records):
+        assert record.levelname == "AUDIT"
+        assert_record_match(record, expected_record)
 
 
-def test_audit_log_repeated_message(caplog):
+def test_do_not_log_popen_env(
+    init_audit_hook,
+    caplog: pytest.LogCaptureFixture,
+    monkeypatch: pytest.MonkeyPatch,
+):
+    monkeypatch.setenv("FOO", "SENSITIVE-DATA")
+    subprocess.Popen(["ls"], env=os.environ)
+    for record in caplog.records:
+        assert "SENSITIVE-DATA" not in str(record.__dict__)
+
+
+def test_do_not_log_request_data(
+    init_audit_hook,
+    caplog: pytest.LogCaptureFixture,
+):
+    data = urllib.parse.urlencode({"foo": "SENSITIVE-DATA"}).encode()
+    req = urllib.request.Request("https://www.python.org", data=data)
+    req.add_header("X-Bar", "SENSITIVE-DATA")
+    try:
+        urllib.request.urlopen(req)
+    except urllib.error.HTTPError:
+        pass
+
+    for record in caplog.records:
+        assert "SENSITIVE-DATA" not in str(record.__dict__)
+
+
+def test_repeated_audit_logs(
+    init_audit_hook, caplog: pytest.LogCaptureFixture, tmp_path: pathlib.Path
+):
     caplog.set_level(logging.INFO)
+    caplog.clear()
 
-    for _i in range(500):
-        audit.audit_log("abc", (1, 2, 3))
+    for _ in range(1000):
+        open(tmp_path / "repeated-audit-logs", "w")
 
-    assert [(r.funcName, r.levelname, r.message, r.count) for r in caplog.records] == [
-        ("audit_log", "AUDIT", "abc", 1),
-        ("audit_log", "AUDIT", "abc", 2),
-        ("audit_log", "AUDIT", "abc", 3),
-        ("audit_log", "AUDIT", "abc", 4),
-        ("audit_log", "AUDIT", "abc", 5),
-        ("audit_log", "AUDIT", "abc", 6),
-        ("audit_log", "AUDIT", "abc", 7),
-        ("audit_log", "AUDIT", "abc", 8),
-        ("audit_log", "AUDIT", "abc", 9),
-        ("audit_log", "AUDIT", "abc", 10),
-        ("audit_log", "AUDIT", "abc", 20),
-        ("audit_log", "AUDIT", "abc", 30),
-        ("audit_log", "AUDIT", "abc", 40),
-        ("audit_log", "AUDIT", "abc", 50),
-        ("audit_log", "AUDIT", "abc", 60),
-        ("audit_log", "AUDIT", "abc", 70),
-        ("audit_log", "AUDIT", "abc", 80),
-        ("audit_log", "AUDIT", "abc", 90),
-        ("audit_log", "AUDIT", "abc", 100),
-        ("audit_log", "AUDIT", "abc", 200),
-        ("audit_log", "AUDIT", "abc", 300),
-        ("audit_log", "AUDIT", "abc", 400),
-        ("audit_log", "AUDIT", "abc", 500),
+    for r in caplog.records:
+        print(r.__dict__["msg"], r.__dict__["count"])
+
+    expected_records = [
+        {"msg": "open", "count": 1},
+        {"msg": "open", "count": 2},
+        {"msg": "open", "count": 3},
+        {"msg": "open", "count": 4},
+        {"msg": "open", "count": 5},
+        {"msg": "open", "count": 6},
+        {"msg": "open", "count": 7},
+        {"msg": "open", "count": 8},
+        {"msg": "open", "count": 9},
+        {"msg": "open", "count": 10},
+        {"msg": "open", "count": 20},
+        {"msg": "open", "count": 30},
+        {"msg": "open", "count": 40},
+        {"msg": "open", "count": 50},
+        {"msg": "open", "count": 60},
+        {"msg": "open", "count": 70},
+        {"msg": "open", "count": 80},
+        {"msg": "open", "count": 90},
+        {"msg": "open", "count": 100},
+        {"msg": "open", "count": 200},
+        {"msg": "open", "count": 300},
+        {"msg": "open", "count": 400},
+        {"msg": "open", "count": 500},
+        {"msg": "open", "count": 600},
+        {"msg": "open", "count": 700},
+        {"msg": "open", "count": 800},
+        {"msg": "open", "count": 900},
+        {"msg": "open", "count": 1000},
     ]
+
+    assert len(caplog.records) == len(expected_records)
+    for record, expected_record in zip(caplog.records, expected_records):
+        assert record.levelname == "AUDIT"
+        assert_record_match(record, expected_record)
+
+
+def assert_record_match(record: logging.LogRecord, expected_record: dict[str, Any]):
+    for key, value in expected_record.items():
+        assert record.__dict__[key] == value
