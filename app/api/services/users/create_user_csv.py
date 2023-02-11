020c4cfa3d45bcbc68d11e34877edd9b70b199a2
Loren Yu <loren@navapbc.com>
2023-02-11 07:52:41 -0800

    Remove unused main function

--- app/api/services/users/create_user_csv.py
+++ app/api/services/users/create_user_csv.py
@@ -83,20 +83,3 @@ def convert_user_records_for_csv(records: list[User]) -> list[UserCsvRecord]:
         )
 
     return out_records
-
-
-def main() -> None:
-    # Initialize DB session / logging / env vars
-    with script_context_manager() as script_context:
-        # Build the path for the output file
-        # This will create a file in the folder specified like:
-        # s3://your-bucket/path/to/2022-09-09-12-00-00-user-roles.csv
-        # The file path can be either S3 or local disk.
-        output_path = os.getenv("USER_ROLE_CSV_OUTPUT_PATH", None)
-        if not output_path:
-            raise Exception("Please specify an USER_ROLE_CSV_OUTPUT_PATH env var")
-
-        file_name = utcnow().strftime("%Y-%m-%d-%H-%M-%S") + "-user-roles.csv"
-        output_file_path = os.path.join(output_path, file_name)
-
-        create_user_csv(script_context.db_session, output_file_path)
