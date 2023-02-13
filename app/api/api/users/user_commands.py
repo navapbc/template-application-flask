4cc0af45426ac2d520bdda656d1668877a673ed7
Loren Yu <loren@navapbc.com>
2023-02-13 13:31:53 -0800

    Add help messages to cmd params

--- app/api/api/users/user_commands.py
+++ app/api/api/users/user_commands.py
@@ -17,8 +17,16 @@ user_blueprint.cli.help = "User commands"
 
 @user_blueprint.cli.command("create-csv", help="Create a CSV of all users and their roles")
 @flask_db.with_db_session
-@click.option("--dir", default=".")
-@click.option("--filename", default=None)
+@click.option(
+    "--dir",
+    default=".",
+    help="Directory to save output file in. Can be an S3 path (e.g. 's3://bucketname/folder/') Defaults to current directory ('.').",
+)
+@click.option(
+    "--filename",
+    default=None,
+    help="Filename to save output file as. Defaults to '[timestamp]-user-roles.csv.",
+)
 def create_csv(db_session: db.Session, dir: str, filename: Optional[str]) -> None:
     if filename is None:
         filename = utcnow().strftime("%Y-%m-%d-%H-%M-%S") + "-user-roles.csv"
