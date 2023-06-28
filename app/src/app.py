1d606f82db634e10276128d81f5f227db824f011
Kyle Gunby <kylegunby@navapbc.com>
2023-06-28 14:15:12 -0500

    Removed index route from openapi spec

--- app/src/app.py
+++ app/src/app.py
@@ -78,9 +78,8 @@ def get_project_root_dir() -> str:
 
 
 def register_index(app: APIFlask) -> None:
-    # When OpenAPI generates a definition for this route, it incorrectly
-    # labels the response content type as application/json.
     @app.route("/")
+    @app.doc(hide=True)
     def index() -> str:
         return """
             <!Doctype html>
