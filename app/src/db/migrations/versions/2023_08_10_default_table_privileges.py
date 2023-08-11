b0845ae1e5d271531f0817d6bf441ef338a55e69
Loren Yu <loren@navapbc.com>
2023-08-11 09:21:22 -0700

    Lint

--- app/src/db/migrations/versions/2023_08_10_default_table_privileges.py
+++ app/src/db/migrations/versions/2023_08_10_default_table_privileges.py
@@ -1,13 +1,10 @@
 """default table privileges
 
 Revision ID: 3ed861176e3d
-Revises: 
+Revises:
 Create Date: 2023-08-10 15:52:10.626153
 
 """
-import sqlalchemy as sa
-from sqlalchemy.dialects import postgresql
-
 from alembic import op
 
 # revision identifiers, used by Alembic.
@@ -18,8 +15,8 @@ depends_on = None
 
 
 def upgrade():
-    op.execute(f"ALTER DEFAULT PRIVILEGES GRANT ALL ON TABLES TO app")
+    op.execute("ALTER DEFAULT PRIVILEGES GRANT ALL ON TABLES TO app")
 
 
 def downgrade():
-    op.execute(f"ALTER DEFAULT PRIVILEGES REVOKE ALL ON TABLES FROM app")
+    op.execute("ALTER DEFAULT PRIVILEGES REVOKE ALL ON TABLES FROM app")
