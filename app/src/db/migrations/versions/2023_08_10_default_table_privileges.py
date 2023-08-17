8edc3e8e57fd918b28284ee80c64c00921c511d9
Loren Yu <loren@navapbc.com>
2023-08-17 14:09:48 -0700

    Explain why default privileges are separate

--- app/src/db/migrations/versions/2023_08_10_default_table_privileges.py
+++ app/src/db/migrations/versions/2023_08_10_default_table_privileges.py
@@ -17,6 +17,11 @@ depends_on = None
 def upgrade():
     # Change default privileges for future objects created by the `migrator`
     # user to automatically be accessible by the `app` user.
+    # This needs to be done as a migration since default privileges for a role
+    # can only be altered by members of the role and not the superuser.
+    # Other permission changes should have already been taken care as part
+    # of the infrastructure setup (creating and configuring the the schema
+    # and roles)
     op.execute("ALTER DEFAULT PRIVILEGES GRANT ALL ON TABLES TO app")
     op.execute("ALTER DEFAULT PRIVILEGES GRANT ALL ON SEQUENCES TO app")
     op.execute("ALTER DEFAULT PRIVILEGES GRANT ALL ON ROUTINES TO app")
