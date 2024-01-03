bc26f0f48bd85ee87607f488ed7e71fbe9cf7859
Sawyer Hollenshead <git@sawyerh.com>
2024-01-03 13:58:33 -0800

    Make update-template.sh more robust to deletions and changes to files (#217)
    ## Ticket
    
    Resolves #196
    
    ## Changes
    
    - Copies the changes made in
    https://github.com/navapbc/template-infra/issues/352

--- template-only-bin/download-and-install-template.sh
+++ template-only-bin/download-and-install-template.sh
@@ -7,5 +7,10 @@ git clone --single-branch --branch main --depth 1 git@github.com:navapbc/templat
 echo "Install template"
 ./template-application-flask/template-only-bin/install-template.sh
 
+# Store template version in a file
+cd template-application-flask
+git rev-parse HEAD >../.template-flask-version
+cd -
+
 echo "Clean up template-application-flask folder"
 rm -fr template-application-flask
