2dba7c00cbf57191553482b8823e9bc0ac3ffd92
Tanner Doshier <git@doshitan.com>
2025-01-07 16:09:36 -0500

    Tag docker-compose stuff with {{app_name}}

--- template/{{app_name}}/bin/wait-for-local-db.sh
+++ template/{{app_name}}/bin/wait-for-local-db.sh
@@ -10,10 +10,14 @@ NO_COLOR='\033[0m'
 MAX_WAIT_TIME=30 # seconds
 WAIT_TIME=0
 
+DB_NAME="${DB_NAME:=${PGDATABASE}}"
+
+DOCKER_DB_SERVICE_NAME="${DOCKER_DB_SERVICE_NAME:=database}"
+
 # Use pg_isready to wait for the DB to be ready to accept connections
 # We check every 3 seconds and consider it failed if it gets to 30+
 # https://www.postgresql.org/docs/current/app-pg-isready.html
-until pg_isready -h localhost -d main-db -q;
+until pg_isready -h localhost -d "${DB_NAME}" -q;
 do
   echo "waiting on Postgres DB to initialize..."
   sleep 3
@@ -21,8 +25,8 @@ do
   WAIT_TIME=$(($WAIT_TIME+3))
   if [ $WAIT_TIME -gt $MAX_WAIT_TIME ]
   then
-    echo -e "${RED}ERROR: Database appears to not be starting up, running \"docker logs main-db\" to troubleshoot.${NO_COLOR}"
-    docker logs main-db
+    echo -e "${RED}ERROR: Database appears to not be starting up, running \"docker logs ${DOCKER_DB_SERVICE_NAME}\" to troubleshoot.${NO_COLOR}"
+    docker logs "${DOCKER_DB_SERVICE_NAME}"
     exit 1
   fi
 done
