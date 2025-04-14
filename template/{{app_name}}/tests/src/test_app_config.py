a7c4c24882256fc584854b73d122696dc5d3c99e
Lisa Chung <943046+lisac@users.noreply.github.com>
2025-04-14 18:00:48 -0400

    fix format

--- template/{{app_name}}/tests/src/test_app_config.py
+++ template/{{app_name}}/tests/src/test_app_config.py
@@ -4,8 +4,8 @@ from unittest import mock
 from src.app_config import AppConfig
 
 
-@mock.patch.dict(os.environ, {"host": '192.123.123.123', "port": '5190'})
+@mock.patch.dict(os.environ, {"host": "192.123.123.123", "port": "5190"})
 def test_app_config_sets_values_from_environment():
-	app_config = AppConfig()
-	assert app_config.host == '192.123.123.123'
-	assert app_config.port == 5190
+    app_config = AppConfig()
+    assert app_config.host == "192.123.123.123"
+    assert app_config.port == 5190
